14703c8800b4ec1c68010a75112ad80cb7dfe08d
huang-me <amos0107@gmail.com>
2020-10-01 15:15:16 +0800

    Experiment to test speed up of __builtin_ctz
    For loops add setbits sequentially, output execution time into .txt file

--- bitset.c
+++ bitset.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 size_t bitset(uint64_t *bitmap, size_t bitmapsize, uint32_t *out)
@@ -35,60 +36,85 @@ size_t naive(uint64_t *bitmap, size_t bitmapsize, uint32_t *out)
 
 int main() {
     uint64_t bitmap[200] = {
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff,
-        0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff};
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000,
+        0x0000000000000000, 0x0000000000000000, 0x0000000000000000};
+
     size_t bitmapsize = 120;
     uint32_t out[200 * 64] = {0};
-    int nums;
-    clock_t start, end;
-    start = clock();
-    nums = bitset(bitmap, bitmapsize, out);
-    end = clock();
-    printf("%d\n", nums);
-    printf("bitset: %f\n", ((double) end - start) / CLOCKS_PER_SEC);
+    srand(time(NULL));
 
-    start = clock();
-    nums = naive(bitmap, bitmapsize, out);
-    end = clock();
-    printf("nums: %d\n", nums);
-    printf("naive: %f\n", ((double) end - start) / CLOCKS_PER_SEC);
+    FILE *naive_file, *ctz_file;
+    naive_file = fopen("naive.txt", "w+");
+    ctz_file = fopen("ctz.txt", "w+");
+
+    for (int temp = 0; temp < 500; temp++) {
+        int random = rand();
+        for (int i = 0; i < bitmapsize; i++) {
+            int shift = rand() % 280;
+            if (shift >= 64)
+                continue;
+            else
+                bitmap[i] |= 1 << shift;
+        }
+
+        for (int k = 0; k < 3; k++) {
+            int nums;
+            clock_t start, end;
+            start = clock();
+            nums = bitset(bitmap, bitmapsize, out);
+            end = clock();
+            // printf("%d\n", nums);
+            // printf("bitset: %f\n", ((double) end - start) / CLOCKS_PER_SEC);
+
+            fprintf(ctz_file, "%d %f\n", nums, ((double) end - start) / CLOCKS_PER_SEC);
+
+            start = clock();
+            nums = naive(bitmap, bitmapsize, out);
+            end = clock();
+            // printf("nums: %d\n", nums);
+            fprintf(naive_file, "%d %f\n", nums, ((double) end - start) / CLOCKS_PER_SEC);
+
+            // printf("naive: %f\n", ((double) end - start) / CLOCKS_PER_SEC);
+        }
+    }
+
     return 0;
 }
